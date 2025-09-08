86147506b472f43a75bb0526829c143d0855fa91
Mikkel Schmidt Nielsen <mikkelschmidt@msn.com>
2025-09-08 14:12:07 +0200

    Made todo page with radzen

---
+++ BlazorApp/Models/Todo.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorApp.Models
+{
+    public class Todo
+    {
+        public string Name { get; set; }
+        public int Time { get; set; }
+    }
+}
--- BlazorApp/Pages/TimerTask.razor.cs
+++ BlazorApp/Pages/TimerTask.razor.cs
@@ -1,6 +1,15 @@
-﻿namespace BlazorApp.Pages
+﻿using BlazorApp.Models;
+using BlazorApp.Shared;
+
+namespace BlazorApp.Pages
 {
     public partial class TimerTask
     {
+        public List<Todo> Tasks { get; set; } = new List<Todo>();
+
+        public void AddTask(string name, int time)
+        {
+            Tasks.Add(new Todo { Name = name, Time = time });
+        }
     }
 }
